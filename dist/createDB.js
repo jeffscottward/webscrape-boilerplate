476d60a03f5f7c7008e974bff7b0e7724c500c77
Jeff Scott Ward <jeffscottward@users.noreply.github.com>
2024-06-19 20:13:10 -0400

    clear dist

--- dist/createDB.js
+++
@@ -1,5 +0,0 @@
-import Database from "better-sqlite3";
-// Initialize the database
-const db = new Database("db/sqlite3.db", { verbose: console.log });
-// Export db for usage in other files
-export default db;
